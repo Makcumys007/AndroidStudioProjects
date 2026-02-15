6fcbdd39cf93ccd4bdb97dacc5e0641f1d9a964d
maxim.abylkassov <maxim.abylkassov@gmail.com>
2026-02-15 14:31:45 +0500

    4.2 Добавление VIewModel в InstagramProfileCard

--- Instagram/app/src/main/java/com/example/instagram/ui/theme/InstagramProfileCard.kt
+++ Instagram/app/src/main/java/com/example/instagram/ui/theme/InstagramProfileCard.kt
@@ -2,44 +2,44 @@ package com.example.instagram.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.instagram.R
 
-@Preview
+
 @Composable
-fun InstagramProfileCard() {
+fun InstagramProfileCard(viewModel: MainViewModel) {
+    val isFollowed: State<Boolean> = viewModel.isFollowing.observeAsState(false)
     Card(
         modifier = Modifier
             .padding(8.dp),
@@ -79,12 +79,34 @@ fun InstagramProfileCard() {
                     "www.facebook.com/emotional_health",
                     fontSize = 14.sp,
                 )
-            Button(
-                onClick = {},
-                ) {
-                Text("Follow")
+            FollowButton(isFollowed.value) {
+                viewModel.changeFollowingStatus()
             }
         }
+    }
+}
+
+@Composable
+private fun FollowButton(
+    isFollowed: Boolean,
+    clickListener: () -> Unit
+) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colorScheme.primary
+            }
+        ),
+        onClick = { clickListener() },
+    ) {
+        val text = if (isFollowed) {
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+        Text(text)
     }
 }
 
@@ -114,6 +136,7 @@ private fun UserStatistic (
     }
 }
 
+/*
 @Preview
 @Composable
 fun PreviewCardLight(){
@@ -129,3 +152,4 @@ fun PreviewCardDark(){
         InstagramProfileCard()
     }
 }
+*/
