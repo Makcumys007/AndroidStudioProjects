fae79bf76dd4a8efb9512717cce33b4ecfb6d9f5
Maxim <maxim.abylkassov@gmail.com>
2026-02-19 07:01:10 +0500

    SwipeToDismissBox удаление свайпом в лево, так же добавил backgroundContent

--- VkNewsClient/app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ VkNewsClient/app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -32,7 +33,10 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
@@ -42,16 +46,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.vknewsclient.MainViewModel
 import com.example.vknewsclient.domain.FeedPost
 import com.example.vknewsclient.domain.StatisticItem
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
@@ -92,6 +99,7 @@ fun MainScreen(viewModel: MainViewModel) {
     ) {
         val feedPosts = viewModel.feedPosts.observeAsState(listOf())
 
+
         LazyColumn(
             contentPadding = PaddingValues(
                 top = 16.dp,
@@ -105,23 +113,57 @@ fun MainScreen(viewModel: MainViewModel) {
                 feedPosts.value,
                 key = { it.id }
             ) { feedPost ->
-                PostCard(
-                    modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background),
-                    feedPost = feedPost,
-                    onViewsClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onLikeClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onShareClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onCommentClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
+                val dismissState = rememberSwipeToDismissBoxState(
+                    confirmValueChange = { value ->
+                        if (value == SwipeToDismissBoxValue.EndToStart) {
+                            viewModel.remove(feedPost)
+                            true
+                        } else {
+                            false
+                        }
+                    }
                 )
+
+
+                SwipeToDismissBox(
+                    modifier = Modifier.animateItem(),
+                    state = dismissState,
+                    enableDismissFromEndToStart = true,
+                    backgroundContent = {
+                        Box(modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize()
+                            .background(Color.Red.copy(0.5f), shape = RoundedCornerShape(16.dp)),
+                            contentAlignment = Alignment.CenterEnd
+                        ) {
+                            Text (
+                                modifier = Modifier.padding(16.dp),
+                                color = Color.White,
+                                text = "Delete",
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                ) {
+
+                    PostCard(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.background),
+                        feedPost = feedPost,
+                        onViewsClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onLikeClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onShareClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onCommentClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                    )
+                }
             }
         }
 
